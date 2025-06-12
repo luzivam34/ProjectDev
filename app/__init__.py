675a0793cfa052e1bd4521a05548bca6af18d93b
Luzy <luzivam.santos24@gmail.com>
2025-06-12 11:33:55 -0300

    estalado uma rota auth.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
-from flask import Flask # type: ignore
+from flask import Flask
 
 def create_app():
     app = Flask(__name__)
 
-    
+    from .routes.auth import auth_bp
+    app.register_blueprint(auth_bp)  
+
+
     
     return app
